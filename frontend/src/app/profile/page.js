5cc1140077f1b68ada8ee96786017056691f9cc4
Sadaru Hansaka <sandaruhansaka0828@gmail.com>
2025-07-13 23:04:40 +0530

    Fixed profile issue

--- frontend/src/app/profile/page.js
+++ frontend/src/app/profile/page.js
@@ -3,7 +3,7 @@ import { useNotes } from "@/components/context/NoteContext";
 import { LogOut } from 'lucide-react';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 
-export default function profile(){
+export default function Profile(){
     const { user, handleLogout } = useNotes();
 
     return(
