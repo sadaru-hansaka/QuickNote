068115216720435a146efac184d86ce02899b7a0
Sadaru Hansaka <sandaruhansaka0828@gmail.com>
2025-07-14 08:51:14 +0530

    Fixed ports and frontend

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ app.get("/", (req, res) => {
 mongoose.connect(process.env.MONGODB_URI)
 .then (()=> {
     console.log("Connected to the DB");
-    app.listen(PORT, ()=> {
+    app.listen(PORT, '0.0.0.0', ()=> {
         console.log(`Server is running on port ${PORT}`);
     })
 })
